45f08929ac8d4ad89a86b395364b6d7baf31eb01
hello-unknown466 <ababa13322978808@foxmail.com>
2026-04-04 21:18:36 +0800

    ababa
    Signed-off-by: hello-unknown466 <ababa13322978808@foxmail.com>

---
+++ c/rank_the_score.c
@@ -0,0 +1,29 @@
+//
+// Created by ababa on 2026/3/30.
+//
+//可以用结构体指针
+#include<stdio.h>
+
+struct Score {
+    int score;
+}s[6];//结构体设置
+
+void point_print(struct Score s[]) {
+    for (int i = 0; i < 6; i++) {
+        if (s[i].score > 90) {//注意数组s[i]表示方式
+            printf("%d\n",s[i].score);
+        }
+    }
+}
+
+int main() {
+    struct Score s[6];
+    printf("give me 6 scores:\n");
+    for (int i = 0; i < 6; i++) {
+        scanf("%d", &s[i].score);
+        printf("\n");
+    }
+    printf("the nicest scores are:\n");
+    point_print(s);//注意数组s只写数组头
+    return 0;
+}
